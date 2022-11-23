5cb71a29b2260b941e4687e4dc274fbba3a75877
vane2810 <olgavanessasf@gmail.com>
2022-11-23 08:20:56 -0600

    Agregar archivos de proyecto.

---
+++ Práctica - Semana 16/Data/Semana 16.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Práctica___Semana_16.Data
+{
+    public class Semana_16
+    {
+    }
+}
