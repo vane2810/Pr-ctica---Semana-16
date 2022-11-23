f43f8b63d096cd6592af8d6e22d545ea0bc242fd
vane2810 <olgavanessasf@gmail.com>
2022-11-23 08:34:07 -0600

    Agregar archivos de proyecto.

---
+++ Práctica - Semana 16/Data/Práctica - Semana 16Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Práctica___Semana_16.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Práctica___Semana_16.Data
+{
+    public class Práctica___Semana_16 
+    {
+        public class Práctica__Semana_16Context : DbContext
+        {
+            public Práctica___Semana_16Context(DbContextOptions<Práctica___Semana_16Context> options) : base (options)
+            {
+
+            }
+        }
+    }
+}
